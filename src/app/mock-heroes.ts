fde6e7798eec5a0df55ce1f533d06d0a54d5f320
Stanley Zheng <dev.stanzheng@gmail.com>
2017-03-22 00:07:16 -0400

    randomally rename a bunch of them

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,10 @@
 import { Hero } from './hero';
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 1, name: 'Spiderman' },
+    { id: 2, name: 'Dare Devil' },
+    { id: 3, name: 'Iron Man' },
+    { id: 4, name: 'Luke Cage' },
+    { id: 5, name: 'Wolverine' },
+    { id: 6, name: 'Captain America' },
+    { id: 7, name: 'Deadpool' }
 ];
