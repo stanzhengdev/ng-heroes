72dc8c9035e427ecbd98cb1acd2860c7a2bfe6dc
Stanley Zheng <dev.stanzheng@gmail.com>
2017-03-23 14:09:58 -0400

    refactor to hero module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
+  declarations: [AppComponent, HeroDetailComponent, HeroComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
